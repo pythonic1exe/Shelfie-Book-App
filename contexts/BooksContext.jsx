7019992de22726dbed03174888a96fba2fa86057
pythonic1exe <wonder.wander19@gmail.com>
2025-10-23 23:42:15 +0500

    Initial commit

---
+++ contexts/BooksContext.jsx
@@ -0,0 +1,114 @@
+import { createContext, useState, useEffect } from "react"
+import { databases,client } from "../lib/appwrite"
+import { ID, Permission, Role, Query } from "react-native-appwrite"
+import { useUser } from "../hooks/useUser"
+
+const DATABASE_ID = "68f96ea600142b860b43"
+const COLLECTION_ID = "68f97dd60003d3d4f6ed"
+
+export const BooksContext = createContext()
+
+export function BooksProvider({children}) {
+  const [books, setBooks] = useState([])
+  const { user } = useUser()
+
+  async function fetchBooks() {
+    try {
+      const response = await databases.listDocuments(
+        DATABASE_ID, 
+        COLLECTION_ID,
+        [
+          Query.equal('userId', user.$id)
+        ]
+      )
+
+      setBooks(response.documents)
+      console.log(response.documents)
+    } catch (error) {
+      console.error(error.message)
+    }
+  }
+
+  async function fetchBookById(id) {
+    try {
+      const response = await databases.getDocument(
+        DATABASE_ID,
+        COLLECTION_ID,
+        id
+      )
+
+      return response
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
+
+  async function createBook(data) {
+    try {
+      await databases.createDocument(
+        DATABASE_ID,
+        COLLECTION_ID,
+        ID.unique(),
+        {...data, userId: user.$id},
+        [
+          Permission.read(Role.user(user.$id)),
+          Permission.update(Role.user(user.$id)),
+          Permission.delete(Role.user(user.$id)),
+        ]
+      )
+    } catch (error) {
+      console.log(error.message)
+      console.log("Failed to create book")
+    }
+  }
+
+  async function deleteBook(id) {
+     try {
+      await databases.deleteDocument(
+        DATABASE_ID,
+        COLLECTION_ID,
+        id,
+      )
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
+
+  useEffect(() => {
+    let unsubscribe
+    const channel = `databases.${DATABASE_ID}.collections.${COLLECTION_ID}.documents`
+
+    if (user) {
+      fetchBooks()
+
+      unsubscribe = client.subscribe(channel, (response) => {
+        const { payload, events } = response
+        console.log(events)
+
+        if (events[0].includes("create")) {
+          setBooks((prevBooks) => [...prevBooks, payload])
+        }
+
+        if (events[0].includes("delete")) {
+          setBooks((prevBooks) => prevBooks.filter((book) => book.$id !== payload.$id))
+        }
+      })
+
+    } else {
+      setBooks([])
+    }
+
+    return () => {
+      if (unsubscribe) unsubscribe()
+    }
+
+  }, [user])
+
+  return (
+    <BooksContext.Provider 
+      value={{ books, fetchBooks, fetchBookById, createBook, deleteBook }}
+    >
+      {children}
+    </BooksContext.Provider>
+  )
+}
